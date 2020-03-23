1f46a1f1e8904b5eba00e3963f2dd0fb97d1bb53
Ubuntu <ubuntu@ip-10-0-0-211.us-gov-west-1.compute.internal>
2020-03-23 16:12:21 +0000

    updates

--- detect-evaluator.py
+++ detect-evaluator.py
@@ -1,14 +1,14 @@
 from imageai.Detection.Custom import DetectionModelTrainer
 
-model = "data/models"
+model = "satboat-yolo/models/final.h5"
 
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
-trainer.setDataDirectory(data_directory="data")
+trainer.setDataDirectory(data_directory="satboat")
 trainer.evaluateModel(
     model_path= model,
-    json_path="data/json/detection_config.json",
+    json_path="satboat-yolo/json/detection_config.json",
     iou_threshold=0.5,
-    object_threshold=0.3,
+    object_threshold=0.001,
     nms_threshold=0.5
 )
--- detect-trainer.py
+++ detect-trainer.py
@@ -1,11 +1,11 @@
 from imageai.Detection.Custom import DetectionModelTrainer
 
 trainer = DetectionModelTrainer()
-trainer.setModelTypeAsRetinaNet()
-trainer.setDataDirectory(data_directory="data")
+trainer.setModelTypeAsYOLOv3()
+trainer.setDataDirectory(data_directory="satboat")
 trainer.setTrainConfig(
-    object_names_array=["boat"],
-    batch_size=32,
+    object_names_array=["satboat"],
+    batch_size=8,
     num_experiments=100,
     train_from_pretrained_model="resnet.h5"
 )
